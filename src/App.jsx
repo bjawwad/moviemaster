7d1962f4449dbc1f2c10b152be368881dde9d29a
Muhammad Bilal Jawwad <mbj786.bilal@gmail.com>
2017-04-15 12:27:12 +0500

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './styles/App.css'
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 import Profile from './Profile';
 import Gallery from './Gallery';
 
@@ -17,25 +17,32 @@ class App extends Component {
     }
 
     searchQuery() {
-        console.log('this.state', this.state);
         const BASE_URL = "https://api.spotify.com/v1/search?";
         let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
         const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
 
-        fetch(FETCH_URL, { method: 'GET' })
+        fetch(FETCH_URL, {method: 'GET'})
             .then(response => response.json())
             .then((json) => {
-                const artist = json.artists.items[0];
-                this.setState({ artist });
+                if (json.artists.items.length > 0) {
+                    const artist = json.artists.items[0];
+                    this.setState({artist});
 
-                FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+                    FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
 
-                fetch(FETCH_URL, { method: 'GET' })
-                    .then(response => response.json())
-                    .then((json) => {
-                        const { tracks } = json;
-                        this.setState({ tracks });
-                    })
+                    fetch(FETCH_URL, {method: 'GET'})
+                        .then(response => response.json())
+                        .then((json) => {
+                            const {tracks} = json;
+                            this.setState({tracks});
+                        })
+                } else {
+                   this.setState({
+                       artist: null,
+                       tracks: null
+                   }) 
+                }
+
             })
     }
 
@@ -53,32 +60,27 @@ class App extends Component {
                             placeholder="Search for an Artist"
                             value={this.state.query}
                             onKeyPress={(event) => {
-                                if (event.key === "Enter" && event.target.value.length > 0)
-                                    this.searchQuery();
+                            if (event.key === "Enter" && event.target.value.length > 0) 
+                                this.searchQuery();
                             }}
                             onChange={(event) => {
-                                this.setState({ query: event.target.value });
-                            }} />
+                            this.setState({query: event.target.value});
+                        }}/>
                         <InputGroup.Addon
                             onClick={() => {
-                                this.searchQuery();
-                            }}>
+                            this.searchQuery();
+                        }}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
-                {
-                    this.state.artist !== null && this.state.tracks !== null
-                        ? <div>
-                            <Profile
-                                artist={this.state.artist}
-                            />
-                            <Gallery 
-                                tracks={this.state.tracks}
-                            />
+                {this.state.artist !== null && this.state.tracks !== null
+                    ? <div>
+                            <Profile artist={this.state.artist}/>
+                            <Gallery tracks={this.state.tracks}/>
                         </div>
-                        : ''
-                }
+                    : 'No Artist found...'
+}
 
             </div>
         )
